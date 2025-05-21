42f3c360c05d65846c0e26a2bc11056f3872adb0
Pratyush240304 <pratyushsharma446@gmail.com>
2025-05-21 13:54:20 +0530

    Creation of a website using basic concepts of Front End Development.
    I have created a website using Front End Technologies like HTML, CSS, JavaScript and Bootstrap. The website is aimed to be responsive and shows basic features of Front End Web Development.

---
+++ Website/refactor.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Custom JS loaded and ready!');
+  });
+  
+  document.querySelector('.btn-primary').addEventListener('click', function () {
+    alert('Thanks for your interest! More info coming soon.');
+  });
+  
